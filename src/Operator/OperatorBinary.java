40192616a1d4378f45fa01456a2b2fd602334126
NailaNY <naylany2@gmail.com>
2020-05-01 22:36:36 +0800

    Operator Binary Assignment

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package Operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Naila Noor Yzzah";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif =3.8;
+
+
+}
