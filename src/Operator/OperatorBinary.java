f29b9182d6a6a530079a93e6fc5821cd5c78c1bf
NailaNY <naylany2@gmail.com>
2020-05-01 22:50:00 +0800

    Operator Binary Aritmatika

--- src/Operator/OperatorBinary.java
+++ src/Operator/OperatorBinary.java
@@ -2,13 +2,20 @@ package Operator;
 
 public class OperatorBinary {
 
-    //operator (=) sebagai assignment/penugasan
-    //menempatkan nilai kedalam variable
-    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+        //operator (=) sebagai assignment/penugasan
+        //menempatkan nilai kedalam variable
+        //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
 
-    String nama = "Naila Noor Yzzah";
-    int jumlahSks = 24;
-    double indeksPrestasiKumulatif =3.8;
+        String nama = "Naila Noor Yzzah";
+        int jumlahSks = 24;
+        double indeksPrestasiKumulatif = 3.8;
 
-
+        public static void main(String[] args){
+            System.out.println(9+3);
+            System.out.println(9-3);
+            System.out.println(9*3);
+            System.out.println(9/3);
+            System.out.println(9%3);
+            System.out.println(9%2);
+    }
 }
