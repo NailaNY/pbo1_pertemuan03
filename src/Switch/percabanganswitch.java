a8ceca1eb7454346d3f549596f69291dc66b29c3
NailaNY <naylany2@gmail.com>
2020-05-01 23:52:29 +0800

    switch

---
+++ src/Switch/percabanganswitch.java
@@ -0,0 +1,27 @@
+package percabanganswitch;
+
+    public class percabanganswitch{
+    public static void main(String[] args) {
+        char nilai='D';
+        switch (nilai){
+            case 'A':
+                System.out.println("Mhs : \"Terimakasih Pak\"");
+                System.out.println("Dosen :\"Selamat ya !!\"");
+                break;
+            case 'B':
+        System.out.println("Mhs : \"Kenapa ulun kada dapat A, pak?\"");
+        System.out.println("Dosen :\"!@#$%\"");
+        break;
+        case 'C':
+        System.out.println("Mhs : \"Ulun izin kada turun kuliah hari ini pak\"");
+        System.out.println("Dosen :\"Behalangan kenapa ikam jadi kada turun kuliah!!\"");
+        break;
+        case 'D':
+        System.out.println("Mhs : \"UTS nya Openbook lah bu?\"");
+        System.out.println("Dosen :\"Close ya, tapi boleh Open Hp!!\"");
+        System.out.println("Mhs : \"yeeeee makasih bu.\"");
+        break;
+        }
+        }
+}
+
